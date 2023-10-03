a286e573cc243fa60162494f53a24d4fba2de2ee
SidraOsman <osmansidra4@gmail.com>
2023-10-03 18:43:05 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let num3 = 16
+let num4 = 6
+let mod = 4
+
+let max = 20
+ 
+Number.isInteger = 50
+let random = 50
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
